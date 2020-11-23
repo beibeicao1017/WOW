826f6d3b74ae2a364fb147f31183dfb6ec8fdad0
beiercao1017 <beiercao1017@ucla.edu>
2020-11-23 18:20:26 +0800

    updated1

--- R/HealthData.R
+++ R/HealthData.R
@@ -1,6 +1,11 @@
-#' WORLD BANK DATA
+#' World Health Data
 #'
+#'@format 264 x 64 dataframe ...
 #'
+#'\describe{
+#'\item{index}{country Names}
+#'\item{column1}{literacy rates}
+#'}
 #'
 "HealthData"
 HealthData = read.csv("World Health Data.csv")
--- R/coco.R
+++ R/coco.R
@@ -1,12 +1,14 @@
 #' coco
+#' #This is a function to test.
 #'
-#' @param a
-#' @param b
+#' @param   FLOAT OR INTEGER > 0
+#' @param   FLOAT OR INTEGER > 0
 #'
-#' @return
-#' @export
+#' @return  FLOAT or INTEGER >0
 #'
 #' @examples
+#' coco(1,2)
+#'
 coco <- function(a,b){
   return(log(a)+log(b))
 }
